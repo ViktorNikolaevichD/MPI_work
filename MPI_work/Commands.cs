d7669239cb02f6175daedc4f7a954d6104c5677e
viktor d <10xl.pro@mail.ru>
2024-01-09 16:30:02 +0300

    Добавлены команды для генерации магазинов и генерации отзывов к магазинам

--- MPI_work/Commands.cs
+++ MPI_work/Commands.cs
@@ -1,10 +1,134 @@
 ﻿using Microsoft.EntityFrameworkCore;
-
-//using MPI_work.Entities;
-
+using MPI_work.Entities;
 namespace MPI_work
 {
     public class Commands
     {
+        // Функция генерации отзывов
+        public static void GenerateFeedback(LocalDb localDb)
+        {
+            // Список магазинов из локальной БД
+            List<Shop> shops = localDb.ShopList.ToList();
+            // Пустой список пользователей для хранения новых пользователей
+            List<User> users = new List<User> { };
+            // Пустой список отзывов для будущего хранения новых отзывов
+            List<Feedback> feedbacks = new List<Feedback> { };
+            // Перебор каждого магазина для добавления отзыва
+            foreach (var shop in shops)
+            {
+                // Случайное число от 1 до 4 включительно
+                var countFeedback = Faker.RandomNumber.Next(1, 4);
+                // Случайные пользователя добавляют отзывы
+                for (int i = 0; i < countFeedback; i++)
+                {
+                    // Генерация экземпляра случайного пользователя
+                    User user = new User
+                    {
+                        FullName = Faker.Name.FullName(),
+                        Age = Faker.RandomNumber.Next(1, 65)
+                    };
+                    // Добавить сгенерированного пользователя в локальный список
+                    users.Add(user);
+                    // Добавить сгенерированного пользователя в локальную БД
+                    localDb.UserList.Add(user);
+
+                    // Сгенерировать случайный отзыв
+                    Feedback feedback = new Feedback
+                    {
+                        // Айди магазина в отзыве
+                        ShopId = shop.ShopId,
+                        Shop = shop,
+                        // Айди пользователя в отзыве
+                        UserId = user.UserId,
+                        User = user,
+                        // Оценка от пользователя
+                        Rating = Faker.RandomNumber.Next(1, 5)
+                    };
+                    // Добавить отзыв в список отзывов
+                    feedbacks.Add(feedback);
+                    // Добавить отзыв в локальную БД
+                    localDb.FeedbackList.Add(feedback);
+                }
+                // Число отзывов для каждого магазина
+                shop.NumberReviews = shop.NumberReviews + feedbacks.Where(p => p.ShopId == shop.ShopId).Count();
+                // Вычислить средний рейтинг у магазина перед выходом(сумма оценок / количество оценок)
+                shop.Rating = (decimal)feedbacks.Where(p => p.ShopId == shop.ShopId).Sum(p => p.Rating) / shop.NumberReviews;
+                        
+            }
+            // Подключение к БД
+            using (var db = new AppDbContext())
+            {
+                // Добавить список пользователей в серверную БД
+                db.Users.AddRange(users);
+                // Обновить данные о магазина в БД
+                db.Shops.UpdateRange(shops);
+                // Добавить отзывы в БД
+                db.Feedbacks.AddRange(feedbacks);
+
+                // Зафиксировать изменения в БД
+                db.SaveChanges();
+            }
+        }
+        // Генерация данных в БД
+        public static void GenerateData(int count)
+        {
+            using (var db = new AppDbContext())
+            {
+                // Генерация магазинов
+                for (int i = 0; i < count; i++)
+                {
+                    // Генерация экземпляра случайного магазина
+                    Shop shop = new Shop
+                    {
+                        Address = Faker.Address.StreetAddress()
+                    };
+                    // Добавить в таблицу магазин
+                    db.Shops.Add(shop);
+                }
+                // Зафиксировать изменения в БД
+                db.SaveChanges();
+            }
+        }
+        // Функция загрузки базы данных
+        public static LocalDb LoadingDb(int rank, int size)
+        {
+            using (var db = new AppDbContext())
+            {
+                // Количество строк в каждой таблице
+                int countShop = db.Shops.Count();
+                int countUser = db.Users.Count();
+                int countFeedback = db.Feedbacks.Count();
+                // Размер части для каждой таблицы
+                int partShop = (countShop / size + 1);
+                int partUser = (countUser / size + 1);
+                int partFeedback = (countFeedback / size + 1);
+                // Смещение по каждой таблице
+                int offsetShop = rank * partShop;
+                int offsetUser = rank * partUser;
+                int offsetFeedback = rank * partFeedback;
+
+                // Вернуть локальную базу данных
+                return new LocalDb
+                {
+                    // Список магазинов
+                    ShopList = db.Shops
+                            .Skip(offsetShop)
+                            .Take(partShop)
+                            .ToList(),
+                    // Список пользователей
+                    UserList = db.Users
+                            .Skip(offsetUser)
+                            .Take(partUser)
+                            .ToList(),
+                    // Список отзывов
+                    FeedbackList = db.Feedbacks
+                            .Include(p => p.User)
+                            .Include(p => p.Shop)
+                            .Skip(offsetFeedback)
+                            .Take(partFeedback)
+                            .ToList()
+                };
+            }
+        }
     }
 }
--- MPI_work/Program.cs
+++ MPI_work/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace MPI_work
+﻿using System.Diagnostics;
+using System.Text.Json;
+
+namespace MPI_work
 {
     internal class Program
     {
@@ -6,7 +9,125 @@
         {
             MPI.Environment.Run(ref args, comm =>
             {
+                // Создаю объект класса, чтобы он был доступени из всех контекстов
+                LocalDb localDb = new LocalDb();
 
+                // Предзагрузка базы данных
+                // Первым загружает базу 0 процесс, чтобы не было проблемы с одновременным созданием базы данных
+                if (comm.Rank == 0)
+                    // База данных для 0 процесса
+                    localDb = Commands.LoadingDb(comm.Rank, comm.Size);
+                // После того как 0 процесс загрузит базу, то все процессы выйдут из барьера
+                comm.Barrier();
+                // Все !0 процессы загрузят базу
+                if (comm.Rank != 0)
+                    // База данных для остальных(не 0) процессов
+                    localDb = Commands.LoadingDb(comm.Rank, comm.Size);
+
+                // Замер времени работы
+                Stopwatch stopWatch = new Stopwatch();
+                // Команда пользователя
+                string? command = null;
+                while (command != "quit")
+                {
+                    // Получение команды от пользователя
+                    if (comm.Rank == 0)
+                    {
+                        Console.Write("Введите команду \ngenerateShops - сгенерировать магазины;" +
+                                                      "\ngenerateFeets - сгенерировать отзывы для магазинов;" +
+                                                      "\ntransfer - перевести игрока в другую команду;" +
+                                                      "\nsalary - изменить зарплату игроку;" +
+                                                      "\nplayer - вывести информацию об игроке;" +
+                                                      "\nteam - вывести состав команды;" +
+                                                      "\ntransfers - вывести список трансферов игрока: ");
+                        command = Console.ReadLine();
+                    }
+
+                    // Рассылка команды по всем процессам
+                    comm.Broadcast(ref command, 0);
+
+                    switch (command)
+                    {
+                        // Перевод игрока в другую команду
+                        case "generateShops":
+                            // Число строк для генерации
+                            int count = 0;
+                            // Число строк для генерации каждым процессом
+                            int countRank = 0;
+                            if (comm.Rank == 0)
+                            {
+                                Console.Write("Какое количество магазинов хотите сгенерировать: ");
+                                count = Convert.ToInt32(Console.ReadLine());
+                                // Если число строк нацело делится на число процессов,
+                                // то поделить и присвоить новое значение,
+                                // если нет, то к результату деления прибавить 1
+                                countRank = count % comm.Size == 0 ? count / comm.Size : count / comm.Size + 1;
+                                // Распределить количество строк для генерации по процессам
+                                for (int i = 1; i < comm.Size; i++)
+                                {
+                                    // Уменьшить число строк для генерации
+                                    count = count - countRank;  
+                                    // Если после уменьшения, число неотрицательноеь
+                                    if (count > -1)
+                                        // Отправить i-ому процессу число строк для генерации
+                                        comm.Send(countRank, i, 0);
+                                    // Иначе разослать всем сообщение с 0
+                                    else 
+                                        comm.Send(0, i, 0);
+                                }
+                                // Установить остаточное значение строк для генерации
+                                countRank = count > 0 ? count : 0;
+                                // Генерация магазинов
+                                Commands.GenerateData(countRank);
+                            }
+                            
+                            if (comm.Rank != 0)
+                            {
+                                // Получение значение строк от 0 процесса
+                                countRank = comm.Receive<int>(0, 0);
+                                // Генерация магазинов
+                                Commands.GenerateData(countRank);
+                            }
+                            // Все процессы ждут окончания генерации
+                            comm.Barrier();
+                            // Обнвление локальной базы данных
+                            localDb = Commands.LoadingDb(comm.Rank, comm.Size);
+
+                            // Все процессы ждут окончания обновления локальной БД
+                            comm.Barrier();
+
+                            if (comm.Rank == 0)
+                                Console.WriteLine("Магазины сгенерированы");
+                            break;
+                        // Сгенерировать каждому магазину по несколько отзывов
+                        case "generateFeets":
+                            if (comm.Rank == 0)
+                                Console.WriteLine("Генерируем отзывы");
+                            // Генерация отзывов
+                            Commands.GenerateFeedback(localDb);
+                            comm.Barrier();
+                            if (comm.Rank == 0)
+                                Console.WriteLine("Отзывы сгенерированы");
+                            break;
+                        // Неизвестная команда
+                        default:
+                            if (comm.Rank == 0 && command != "quit")
+                                Console.WriteLine("Неизвестная команда");
+                            break;
+                    }
+
+                    if (comm.Rank == 0)
+                    {
+                        // Вывод времени выполнения
+                        TimeSpan ts = stopWatch.Elapsed;
+                        string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                        ts.Hours, ts.Minutes, ts.Seconds,
+                        ts.Milliseconds / 10);
+                        Console.WriteLine($"RunTime {comm.Rank} " + elapsedTime);
+                    }
+                    // Барьер, чтобы все процессы подождали, пока 0 процесс выведет время работы
+                    comm.Barrier();
+                }
             });
         }
     }
